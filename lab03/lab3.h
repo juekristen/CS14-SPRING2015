0eac6c064a6b785543e26924e30e6ee3d93d9a99
juekristen <kjue001@ucr.edu>
2015-04-27 17:33:36 +0000

    lab3.h finished

--- lab03/lab3.h
+++ lab03/lab3.h
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <stack>
+#include <cstdlib>
 using namespace std;
+template <typename T>
 class TwoStackFixed
 {
     public:
         TwoStackFixed(int size, int maxtop )
-            :SIZE(size), MAX(maxtop), stack1(0), stack2(0)
-        {}
+            :SZ(size), MAX(maxtop)
+        {
+            fullarr = new T[size];
+           
+        }
         void pushStack1(T value) //to push element into first stack.
         {
             if(stack1.size() <= MAX)
@@ -14,20 +19,31 @@ class TwoStackFixed
                 stack1.push(value);
                 fullarr[countS1] = value;
                 countS1++;
+                return;
             }
+            cout << "Overflowed!\n";
+            exit(1);
             
         }
         void pushStack2(T value) //to push element into second stack.
         {
-           if(stack2.size() < SIZE - MAX) 
+           if(stack2.size() < SZ - MAX) 
            {
                 stack2.push(value);
                 fullarr[countS2] = value;
                 countS2 --;
+                return;
            }
+           cout << "Overflowed!\n";
+           exit(1);
         }
         T popStack1() //to pop element from first stack.
         {
+            if(isEmptyStack1())
+            {
+                cout << "Cannot pop empty stack:\n";
+                exit(1);
+            }
             T temp = stack1.front();
             stack1.pop();
             fullarr[MAX] = T();
@@ -35,6 +51,11 @@ class TwoStackFixed
         }
         T popStack2()//to pop element from second stack.
         {
+            if(isEmptyStack2())
+            {
+                cout << "Cannot pop empty stack:\n";
+                exit(1);
+            }
             T temp = stack2.front();
             stack2.pop();
             fullarr[MAX+1] = T();
@@ -48,7 +69,7 @@ class TwoStackFixed
         }
         bool isFullStack2() //helper function to check whether second stack is full.
         {
-            if(stack2.size() == SIZE - MAX)
+            if(stack2.size() == SZ - MAX)
             return true;
             return false;
         }
@@ -66,12 +87,154 @@ class TwoStackFixed
             return true;
             return false;
         }
+        void display()
+        {
+            while(stack1.size()!=0)
+            {
+                cout << stack1.top()<< ' ';
+                stack1.pop();
+            }
+            cout << endl;
+            while(stack2.size()!=0)
+            {
+                cout << stack2.top()<< ' ';
+                stack2.pop();
+            }
+            cout << endl;
+            
+        }
+    private:
+        const int SZ;
+        T *fullarr;
+        const int MAX;
+        stack<T> stack1;
+        stack<T> stack2;
+        int countS1 = 0;
+        int countS2 = SZ - 1;
+};
+template <typename T>
+class TwoStackOptimal
+{   public:
+        TwoStackOptimal(int size) //constructor that constructs array of size and
+                                    //constructs empty stacks.
+            :SIZE(size)
+        {
+            fullarr = new T[size];
+        }
+        void pushFlexStack1(T value) //to push element into first stack.
+        {
+            if(isFullStack1())
+            {
+                cout << "Overflowed!\n";
+                exit(1);
+            }
+            stack1.push(value);
+            fullarr[countS1] = value;
+            countS1++;
+        }
+        void pushFlexStack2(T value) //to push element into second stack.
+        {
+            if(isFullStack2())
+            {
+                cout << "Overflowed!\n";
+                exit(1);
+            }
+            stack2.push(value);
+            fullarr[countS2] = value;
+            countS2 --;
+        }
+        T popFlexStack1() //to pop element into first stack.
+        {
+             if(isEmptyStack1())
+            {
+                cout << "Cannot pop empty stack:\n";
+                exit(1);
+            }
+            T temp = stack1.front();
+            stack1.pop();
+            fullarr[countS1] = T();
+            return temp;
+        }
+        T popFlexStack2() //to pop element from second stack.
+        {
+             if(isEmptyStack2())
+            {
+                cout << "Cannot pop empty stack:\n";
+                exit(1);
+            }
+            T temp = stack1.front();
+            stack1.pop();
+            fullarr[countS1] = T();
+            return temp;
+        }
+        bool isFullStack1()//helper function to check whether first stack is full.
+        {
+            if(stack1.size() + stack2.size() == SIZE)
+                return true;
+                return false;
+        }
+        bool isFullStack2() //helper function to check whether second stack is full.
+        {
+            if(stack1.size() + stack2.size() == SIZE)
+                return true;
+                return false;
+        }
+        bool isEmptyStack1() //helper function to check whether first stack is empty.
+        {
+            if(stack1.size()==0)
+            return true;
+            return false;
+        }
+        bool isEmptyStack2() //helper function to check whether second stack is empty.
+        {
+            if(stack2.size()==0)
+            return true;
+            return false;
+        }
+        void display()
+        {
+            while(stack1.size()!=0)
+            {
+                cout << stack1.top()<< ' ';
+                stack1.pop();
+            }
+            cout << endl;
+            while(stack2.size()!=0)
+            {
+                cout << stack2.top()<< ' ';
+                stack2.pop();
+            }
+            cout << endl;
+            
+        }
     private:
         const int SIZE;
-        T fullarr[SIZE];
-        const int MAX;
+        T *fullarr;
         stack<T> stack1;
         stack<T> stack2;
         int countS1 = 0;
         int countS2 = SIZE - 1;
 };
+template <typename T>
+void helpShowTowerStates(int n, stack<T>& A, stack<T>& B, stack<T>& C,char one, 
+    char two, char three)
+{
+    if(n == 1)
+    {
+        cout << "Moved " <<  A.top() << " from peg "<< one << " to " << three << endl;
+        C.push(A.top());
+        A.pop();
+    }
+    else
+    {
+        helpShowTowerStates(n-1,A,C,B,one,three,two);
+        helpShowTowerStates(1,A,B,C,one,two,three);
+        helpShowTowerStates(n-1,B,A,C,two,one,three);
+    }
+}
+template <typename T>
+void showTowerStates(int n, stack<T>& A, stack<T>& B, stack<T>& C)
+{
+    helpShowTowerStates(n,A,B,C,'A','B','C');
+}
+
